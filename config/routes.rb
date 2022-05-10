c57316c59641fa552e1774bf3f1dba6745445301
yukiharwood <yukiharwood@outlook.com>
2022-05-10 13:06:22 +0900

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb '/path', to: 'controller#action' as :prefix
+  get '/tasks', to: 'tasks#index', as: :tasks
 end
