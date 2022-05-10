2709cdb3284c2622e3a62a4b13721f751db4aa60
yukiharwood <yukiharwood@outlook.com>
2022-05-10 15:01:49 +0900

    linked show and index pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb '/path', to: 'controller#action' as :prefix
   get '/tasks', to: 'tasks#index', as: :tasks
+  get '/task/:id', to: 'tasks#show', as: :task
 end
