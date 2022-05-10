e8ce6c8c28bd514aba20befd0c3e680ab0f3badb
yukiharwood <yukiharwood@outlook.com>
2022-05-10 23:42:52 +0900

    added delete function and links between tasks and tasks/new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,22 @@ class TasksController < ApplicationController
 
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+
+  def destroy
+    puts 'destroy?'
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   get '/task/:id', to: 'tasks#show', as: :task
   get '/tasks/new/', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch'/task/:id', to: 'tasks#update'
+  delete '/task/:id', to: 'tasks#destroy'
 end
