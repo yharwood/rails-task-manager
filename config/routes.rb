40d6a3c89b7df1faf09d47a9cb9717b643b2efe3
yukiharwood <yukiharwood@outlook.com>
2022-05-10 16:30:29 +0900

    completed on adding tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # verb '/path', to: 'controller#action' as :prefix
   get '/tasks', to: 'tasks#index', as: :tasks
   get '/task/:id', to: 'tasks#show', as: :task
+  get '/tasks/new/', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
 end
